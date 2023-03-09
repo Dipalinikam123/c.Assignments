90878bf705737eb82b65142c43a73e8bcf71dc2a
Dipalinikam123 <127381417+Dipalinikam123@users.noreply.github.com>
2023-03-09 15:18:21 +0530

    Add files via upload

---
+++ Deepali.c/printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  printf("\nEnter your name:- Dipali Nikam.\n\n");
+  
+  printf("Enter your Birth date:- 1/8/2001\n\n");
+  
+  printf("Enter your age:- 21\n\n");
+  
+  printf("Enter your address:- At.po.Borad, Dist:-Nandurbar, Maharashtra");
+ return 0;  
+} 
